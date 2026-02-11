390d858d98c8b9547e42d323d32b5e3371bb4e84
Aniket <aniketpratapsingh7310@gmail.com>
2026-02-11 19:38:46 +0530

    gcs configs

--- src/config/gcs.js
+++ src/config/gcs.js
@@ -3,15 +3,17 @@ import path from "path";
 
 // Path to your service account key JSON
 const keyPath = path.join(process.cwd(), "./gcs-key.json");
-
+const ProjectID = process.env.GCSPROJECTID
 // Init Storage client
 const storage = new Storage({
   keyFilename: keyPath,
-  projectId: process.env.GCSPROJECTID, // replace with actual project ID
+  projectId: ProjectID, // replace with actual project ID
 });
 
+
+const BucketName = process.env.GCSBUCKETNAME; // replace with your bucket name
 // Bucket reference
-const bucketName = process.env.GCSBUCKETNAME; // replace with your bucket name
+const bucketName = BucketName; // replace with your bucket name
 const bucket = storage.bucket(bucketName);
 
 export default bucket;
