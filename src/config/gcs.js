1d762228a8dead6996a1d6e8120eca70ef838ee8
Aniket <aniketpratapsingh7310@gmail.com>
2026-02-11 19:29:30 +0530

    gcs configurations

--- src/config/gcs.js
+++ src/config/gcs.js
@@ -7,11 +7,11 @@ const keyPath = path.join(process.cwd(), "./gcs-key.json");
 // Init Storage client
 const storage = new Storage({
   keyFilename: keyPath,
-  projectId: "lowercase-472906", // replace with actual project ID
+  projectId: process.env.GCSPROJECTID, // replace with actual project ID
 });
 
 // Bucket reference
-const bucketName = "lowercase_albums_photos_uat"; // replace with your bucket name
+const bucketName = process.env.GCSBUCKETNAME; // replace with your bucket name
 const bucket = storage.bucket(bucketName);
 
 export default bucket;
